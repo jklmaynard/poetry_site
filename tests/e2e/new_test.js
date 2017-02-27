be157260e5ac3ea578ae182513c6f3b2f1254fff
James Maynard <jameskirkmaynard@gmail.com>
2017-02-27 08:52:19 -0800

    upload nightwatch and selenium-download, set up first test

---
+++ tests/e2e/new_test.js
@@ -0,0 +1,12 @@
+var config = require('../../nightwatch.conf.js');
+var SCREENSHOT_PATH = "screenshots";
+
+module.exports = {
+  'New Test Attempt': function(browser) {
+    browser.url('localhost:3000');
+    browser.waitForElementVisible('body');
+    browser.assert.title('James Maynard | Poetry');
+    browser.saveScreenshot(SCREENSHOT_PATH + '/test_page_screencap.png');
+    browser.end();
+  }
+};
