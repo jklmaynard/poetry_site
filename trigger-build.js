b1fb6099254daf0f69f013ff1474461ffcfeb77f
James Maynard <jameskirkmaynard@gmail.com>
2017-04-17 16:01:23 -0700

    add google analytics

--- trigger-build.js
+++ trigger-build.js
@@ -1,8 +1,8 @@
 const Travis = require('travis-ci');
-
 const travis = new Travis({
   version: '2.0.0'
 })
+
 travis.authenticate({github_token: process.env.GH_TOKEN}, function(err, res) {
   if (err) {
     console.log(err);
